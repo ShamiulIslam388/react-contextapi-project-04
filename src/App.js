861d0c4df55b8efed03a357bdcc313baf3f1457f
ShamiulIslam388 <58252309+ShamiulIslam388@users.noreply.github.com>
2021-03-08 17:53:46 +0600

    hello messsage just

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ export default function App() {
   console.log(myContext.Consumer);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Context api!!!</h1>
     </div>
   );
 }
